0215d43b17508e73f4c640e2f6849873bbf89268
limhokyun <lim@nsmarts.co.kr>
2022-03-04 11:00:00 +0900

    ts 코드 정리

--- src/app/components/board-fabs/board-fabs.component.ts
+++ src/app/components/board-fabs/board-fabs.component.ts
@@ -6,42 +6,42 @@ import { ViewInfoService } from 'src/@pv/store/view-info.service';
 
 
 @Component({
-  selector: 'app-board-fabs',
-  templateUrl: './board-fabs.component.html',
-  styleUrls: ['./board-fabs.component.scss']
+    selector: 'app-board-fabs',
+    templateUrl: './board-fabs.component.html',
+    styleUrls: ['./board-fabs.component.scss']
 })
 export class BoardFabsComponent implements OnInit {
 
-  constructor(
-    private viewInfoService: ViewInfoService,
-    private zoomService: ZoomService
+    constructor(
+        private viewInfoService: ViewInfoService,
+        private zoomService: ZoomService
 
-  ) { }
+    ) { }
 
-  ngOnInit(): void {
+    ngOnInit(): void {
 
-  }
+    }
 
 
-  /**
-   * Zoom Button에 대한 동작
-   * - viewInfoService의 zoomScale 값 update
-   *
-   * @param action : 'fitToWidth' , 'fitToPage', 'zoomIn', 'zoomOut'
-   */
-  clickZoom(action:any){
-    console.log(">> Click Zoom: ", action);
-    const pageInfo = this.viewInfoService.state.pageInfo;
-    //document Number -> 1부터 시작.
-    const docNum = pageInfo.currentDocNum;
-    const currentPage = pageInfo.currentPage;
-    const prevZoomScale = pageInfo.zoomScale;
+    /**
+     * Zoom Button에 대한 동작
+     * - viewInfoService의 zoomScale 값 update
+     *
+     * @param action : 'fitToWidth' , 'fitToPage', 'zoomIn', 'zoomOut'
+     */
+    clickZoom(action: any) {
+        console.log(">> Click Zoom: ", action);
+        const pageInfo = this.viewInfoService.state.pageInfo;
+        //document Number -> 1부터 시작.
+        const docNum = pageInfo.currentDocNum;
+        const currentPage = pageInfo.currentPage;
+        const prevZoomScale = pageInfo.zoomScale;
 
-    const newZoomScale = this.zoomService.calcZoomScale(action, docNum, currentPage, prevZoomScale);
-    console.log(newZoomScale)
+        const newZoomScale = this.zoomService.calcZoomScale(action, docNum, currentPage, prevZoomScale);
+        console.log(newZoomScale)
 
-    this.viewInfoService.updateZoomScale(newZoomScale);
+        this.viewInfoService.updateZoomScale(newZoomScale);
 
-  }
+    }
 
 }
--- src/app/components/board-slide-view/board-slide-view.component.spec.ts
+++ src/app/components/board-slide-view/board-slide-view.component.spec.ts
@@ -3,23 +3,23 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BoardSlideViewComponent } from './board-slide-view.component';
 
 describe('BoardSlideViewComponent', () => {
-  let component: BoardSlideViewComponent;
-  let fixture: ComponentFixture<BoardSlideViewComponent>;
+    let component: BoardSlideViewComponent;
+    let fixture: ComponentFixture<BoardSlideViewComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BoardSlideViewComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [BoardSlideViewComponent]
+        })
+            .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BoardSlideViewComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(BoardSlideViewComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
